778eb5712ede9c5a94d725572e2e5f21f0afaf37
ANISDHIYAEDDINE <dhiaeboudiaf@gmail.com>
2020-11-20 22:56:52 +0100

    test push

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> Hello world just to test TravisConfig whoaaah okey  .. </p>
+        <p>just to test TravisConfig whoaaah okey  .. </p>
         <a
           className="App-link"
           href="https://reactjs.org"
